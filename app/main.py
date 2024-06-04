12bc7f929988d6b129bdea2223b6c7076aa67806
Vi <for29you@gmail.com>
2024-06-04 13:58:15 +0300

    Пример env

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.api.routers import main_router
 from app.core.config import settings
+from app.core.google_client import cred
 from app.core.init_db import create_first_superuser
 
 app = FastAPI(
@@ -13,4 +14,13 @@ app.include_router(main_router)
 
 @app.on_event('startup')
 async def startup():
+
+    print('*' * 30)
+    print(settings.private_key)
+    print('*' * 30)
+
+    print('1' * 30)
+    print(cred)
+    print('1' * 30)
+
     await create_first_superuser()
